37115e659f3beb4bc864573811148dbb81ae0b05
Haunted-Cpp <56511128+Haunted-Cpp@users.noreply.github.com>
2023-06-24 03:52:20 +0100

    Update ESU.hpp

--- ESU.hpp
+++ ESU.hpp
@@ -10,29 +10,36 @@ private:
   static std::vector< std::vector<int> > g;
   static int K; // Subgraph size
   static int V; // Initial node
-  static void enumerateSubgraphs(std::vector<int>);
+  
+  static const int CLASSES = 180;
+
+  static int SC;
+  static int EC;
+  static void enumerateSubgraphs(std::vector<int>&);
   static void setupAndRun(const std::vector< std::vector<int> >& , int);
   static Hypergraph h;
-  static std::map< int, long long> counterHyper;
+  //static std::map< int, long long> counterHyper;
+  static vector<long long> counterHyper;
   static std::unordered_set< std::vector<int>, HashFunction > visited;
   static std::map<std::string, long long> FaSE(const std::vector<std::pair<int, int> > edges, int k);
   static Hypergraph binaryToHyper(std::string str, int k);
   static void clearDataStruct();
+  static void computeSize(int motifSize);
   static std::vector< std::vector< std::pair<int, int> > > startEdgeGraphSubgraphs(Hypergraph&, int);
   static std::vector< std::pair<long long, std::string> > getEquivalenceClass(const std::vector< std::vector<int> >&, int);
   static std::vector< std::vector< std::pair<int, int> > > getAllSubgraphs(const std::vector< std::vector<int> >&, int);
-  static std::map< int, long long> k3(Hypergraph&);
-  static std::map< int, long long> k3Fase(Hypergraph&);
-  static std::map< int, long long> k3Triangle(Hypergraph& inputGraph);
-  static std::map< int, long long> k3ESU(Hypergraph& inputGraph);
-  static std::map< int, long long> bruteForce3(Hypergraph& inputGraph);
-  static std::map< int, long long> k4(Hypergraph&);
-  static std::map< int, long long> k4Fase(Hypergraph&);
-  static std::map< int, long long> k4ESU(Hypergraph& inputGraph);
-  static std::map< int, long long> bruteForce4(Hypergraph& inputGraph);
+  static std::vector<long long> k3(Hypergraph&);
+  static std::vector<long long> k3Fase(Hypergraph&);
+  static std::vector<long long> k3Triangle(Hypergraph& inputGraph);
+  static std::vector<long long> k3ESU(Hypergraph& inputGraph);
+  static std::vector<long long> bruteForce3(Hypergraph& inputGraph);
+  static std::vector<long long> k4(Hypergraph&);
+  static std::vector<long long> k4Fase(Hypergraph&);
+  static std::vector<long long> k4ESU(Hypergraph& inputGraph);
+  static std::vector<long long> bruteForce4(Hypergraph& inputGraph);
   static void k3IntermediateForm(Hypergraph& inputGraph);
   static void k4IntermediateForm(Hypergraph& inputGraph);
-  static void printResults(std::chrono::time_point<std::chrono::steady_clock>, std::chrono::time_point<std::chrono::steady_clock>, std::map< int, long long>, int, bool, std::ostream&);
+  static void printResults(std::chrono::time_point<std::chrono::steady_clock>, std::chrono::time_point<std::chrono::steady_clock>, std::vector<long long>, int, bool, std::ostream&);
 public:
   static void networkCensus(Hypergraph&, int, bool, int, std::ostream&);
   static void findMotifs(Hypergraph&, int, bool, bool, int, int, int, std::ostream&);
